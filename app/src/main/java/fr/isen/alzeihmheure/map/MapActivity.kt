95ec68e1196afe5c534fae2a7d574c3a059caa4e
airline <hubert.laury@hotmail.fr>
2021-02-16 11:23:52 +0100

    spinner

---
+++ app/src/main/java/fr/isen/alzeihmheure/map/MapActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.alzeihmheure.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.isen.alzeihmheure.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
