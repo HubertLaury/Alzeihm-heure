8b2e48cfb30fcbeaae7e0dea61a326cca70bbcfc
QuignonDorian <dorian.quignon@isen.yncrea.fr>
2021-04-30 16:03:18 +0200

    new member

--- app/src/main/java/fr/isen/alzeihmheure/member/User.kt
+++ app/src/main/java/fr/isen/alzeihmheure/member/User.kt
@@ -2,11 +2,12 @@ package fr.isen.alzeihmheure.member
 
 import java.io.Serializable
 
-class User(val lastname: String = "",
-           val email: String = "",
-           val adresse: String = "",
-           val firstname: String = "",
-           val telephone: String = "",
-           val picture: String = "",
-           val password: String = ""): Serializable {
+class User(
+        val lastname: String? = "",
+        val email: String = "",
+        val adresse: String = "",
+        val firstname: String = "",
+        val telephone: String = "",
+        val picture: String = "",
+        val password: String = ""): Serializable {
 }
