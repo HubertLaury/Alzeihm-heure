cea8e1f60a5a9e610e3d9a6f85caaf3da04c427c
airline <hubert.laury@hotmail.fr>
2021-05-11 15:41:36 +0200

    nul

--- app/src/main/java/fr/isen/alzeihmheure/member/AddMemberActivity.kt
+++ app/src/main/java/fr/isen/alzeihmheure/member/AddMemberActivity.kt
@@ -12,7 +12,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.android.gms.tasks.Continuation
 import com.google.android.gms.tasks.Task
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
@@ -46,20 +49,22 @@ class AddMemberActivity : AppCompatActivity() {
 
         format = findViewById(R.id.format)
         btnImport = findViewById(R.id.btnImport)
+        buttonUpload = findViewById(R.id.btnUpload)
         picture = findViewById(R.id.picture)
         val editText = findViewById<EditText>(R.id.nom)
 
-        binding.btnImport.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val name: String = editText.getText().toString()
-                binding.format.setText(name)
-                selectPicture()
-            }
-        })
+        binding.btnImport.setOnClickListener {
+            val name: String = editText.getText().toString()
+            binding.format.setText(name)
+            selectPicture()
+        }
+
+        binding.btnUpload.setOnClickListener {
+                uploadImage()
+        }
 
         btnAdd = findViewById(R.id.btnAdd)
         binding.btnAdd.setOnClickListener {
-            uploadImage()
             when {
                 //si rien est saisie pour le prenom on affiche un message grace à un toast
                 TextUtils.isEmpty(binding.firstname.text.toString().trim { it <= ' ' }) -> {
@@ -118,36 +123,38 @@ class AddMemberActivity : AppCompatActivity() {
                  val adresse: String = binding.adresse.text.toString()
                  val email: String = binding.email.text.toString()
                  val telephone: String = binding.telephone.text.toString()
-                 val ImageStore = FirebaseStorage.getInstance().reference.child("uploads/").downloadUrl
-                 val url = "https://firebasestorage.googleapis.com/v0/b/alzheimheure.appspot.com/o/uploads%2Fmtp.jpg?alt=media&token=37833540-7b87-4d49-bdd8-922133aa00ac"
 
-                 ImageStore.addOnSuccessListener { uri -> //Setting the image to imageView using Glide Library
-                     Glide.with(binding.picture.context).load(url).into(picture)
-                 }
 
                  val firebaseDatabase = FirebaseDatabase.getInstance().getReference("users")
                  val uploadId = firebaseDatabase.push().key
+                 val ref = storageReference!!.child("uploads/app.png").downloadUrl
 
-                 // we are use add value event listener method
-                 // which is called with database reference.
-                 firebaseDatabase.addValueEventListener(object : ValueEventListener {
-                     override fun onDataChange(snapshot: DataSnapshot) {
-                         // inside the method of on Data change we are setting
-                         // our object class to our database reference.
-                         // data base reference will sends data to firebase.
-                         val user = User(firstname, lastname, email, telephone, adresse, picture.toString())
-                         firebaseDatabase.child(uploadId!!).setValue(user)
+                         ref.addOnSuccessListener {
+                     // Got the download URL for 'users/me/profile.png'
+                             val lien = ref.toString()
 
-                         // after adding this data we are showing toast message.
-                         Toast.makeText(this@AddMemberActivity, "data added", Toast.LENGTH_SHORT).show()
-                     }
+                     firebaseDatabase.addValueEventListener(object : ValueEventListener {
+                         override fun onDataChange(snapshot: DataSnapshot) {
+                             // inside the method of on Data change we are setting
+                             // our object class to our database reference.
+                             // data base reference will sends data to firebase.
+                             val user = User(firstname, lastname, email, telephone, adresse, lien)//changer picture avec la nouvelle variable
+                             firebaseDatabase.child(uploadId!!).setValue(user)
 
-                     override fun onCancelled(error: DatabaseError) {
-                         // if the data is not added or it is cancelled then
-                         // we are displaying a failure toast message.
-                         Toast.makeText(this@AddMemberActivity, "Fail to add data $error", Toast.LENGTH_SHORT).show()
-                     }
-                 })
+                             // after adding this data we are showing toast message.
+                             Toast.makeText(this@AddMemberActivity, "data added", Toast.LENGTH_SHORT).show()
+                         }
+
+                         override fun onCancelled(error: DatabaseError) {
+                             // if the data is not added or it is cancelled then
+                             // we are displaying a failure toast message.
+                             Toast.makeText(this@AddMemberActivity, "Fail to add data $error", Toast.LENGTH_SHORT).show()
+                         }
+                     })
+
+                 }.addOnFailureListener {
+                     // Handle any errors
+                 }
             }
             }
         }
@@ -158,7 +165,7 @@ class AddMemberActivity : AppCompatActivity() {
         val data = HashMap<String, Any>()
         data["imageUrl"] = uri
 
-        db.collection("")
+        db.collection("uploads")
                 .add(data)
                 .addOnSuccessListener { documentReference ->
                     Toast.makeText(this, "Saved to DB", Toast.LENGTH_LONG).show()
